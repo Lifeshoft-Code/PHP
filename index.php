f380da8a794c12af2a96f235a9cb05e5b3de48d8
unknown <Jonathanlora@gmail.com>
2022-03-29 12:05:20 -0400

    version 0.1.3: agregandando datos en php y variables

--- index.php
+++ index.php
@@ -5,17 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="public\css\normalize.css">
 </head>
 <body>
     <!-- Para escribir codigo php -->
+    <section>
+        <?php
+            /*Mostrar por mensaje por pantalla */    
+            print "<h2>Hola Mundo PHP con print</h2> <br>";
+            echo "otra forma de mostrar Hola Mundo PHP con echo";
+            /* la diferencia es que echo y print es que echo admite lo siguiente */
+            echo "<br>", "Otra forma", "<br>", "<h3>echo vs print</h3>";
+        ?>
+        <img src="./public/assets/img/diferencia-echo-and-print/echovspring.png" alt="">
+    </section>
     
-    <?php
-        /*Mostrar por mensaje por pantalla */    
-        print "<h2>Hola Mundo PHP con print</h2> <br>";
-        echo "otra forma de mostrar Hola Mundo PHP con echo";
-        /* la diferencia es que echo y print es que echo admite lo siguiente */
-        echo "<br>", "Otra forma", "<br>", "<h3>echo vs print</h3>";
-    ?>
-    <img src="./public/assets/img/diferencia-echo-and-print/echovspring.png" alt="">
+    <hr>
+    <section>
+        <h3>Tipos de datos y variables</h3>
+        <ul>
+            <li>Enteros</li>
+            <li>Flotante</li>
+            <li>String (Cadena de caracteres)</li>
+            <li>Booleanos</li>
+        </ul>
+
+        <?php
+
+        ?>
+    </section>
 </body>
 </html>
